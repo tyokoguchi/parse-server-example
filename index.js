1f711115cee2dcb8b261b4308f50b844e0145a57
tyokoguchi <37196142+tyokoguchi@users.noreply.github.com>
2018-03-09 12:17:52 +0900

    update without pw reset

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://ccecere12:7ig2bmv1a@ds025399.mlab.com:25399/cafecura';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '2np9unq9n23baoasijdn2',
+  masterKey: process.env.MASTER_KEY || 'p12u4hrufnqci34j3084n1p2nq1', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://cafecura.herokuapp.com/',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
