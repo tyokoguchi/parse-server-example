d3c0c075a17607f4c35b52d8a21f3da243e3de1a
tyokoguchi <37196142+tyokoguchi@users.noreply.github.com>
2018-03-13 07:06:53 +0900

    changed appID

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'wen12398nvoenp20fn20f',
+  appId: process.env.APP_ID || 'cafecura',
   masterKey: process.env.MASTER_KEY || '28doqnt9qnsoqwnie02a',
   serverURL: process.env.SERVER_URL || 'https://cafecura.herokuapp.com/',
   appName : 'cafecura',
